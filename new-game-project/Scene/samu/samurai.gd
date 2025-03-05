9877e1906cc81df37ae3fdaff844a353a6bb85be
Drake <mkanyal001@gmail.com>
2025-03-06 00:53:15 +0530

    Added Double Jump
    + double Jump and animnation
    = bug while jumping after being idle, unable to move

--- new-game-project/Scene/samu/samurai.gd
+++ new-game-project/Scene/samu/samurai.gd
@@ -4,18 +4,20 @@ extends CharacterBody2D
 @onready var player = $Sprite2D
 
 @export var SPEED = 300.0
+@export var max_jump = 2
+
 const JUMP_VELOCITY = -220.0
 const dash_velocity = 400
-
+var jump_count =  0
 var input
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
 func _physics_process(delta):
 	moving(delta)
-	
 func moving(delta):
 	input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	if input != 0:
+	if input != 0 and is_on_floor():
 		anim.play("run")
 		if input > 0 :
 			player.flip_h = false
@@ -29,8 +31,21 @@ func moving(delta):
 		velocity.x = 0
 		anim.play("idle")
 	
-	add_gravity()
+	if !is_on_floor():
+		anim.play("jump_1")
+	if is_on_floor():
+		jump_count = 0
+	if Input.is_action_just_pressed("jump") && is_on_floor() && jump_count < 1 : 
+		jump_count += 1
+		velocity.y = JUMP_VELOCITY*4.5
+	if Input.is_action_just_pressed("jump") && !is_on_floor() && jump_count < max_jump:
+		jump_count += 1
+		velocity.y = JUMP_VELOCITY*4
+		
+ 
+	add_gravity(delta)
 	move_and_slide()	
 
-func add_gravity():
-	velocity.y+=gravity
+func add_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity *delta *3
